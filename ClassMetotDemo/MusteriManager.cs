485db252aa016ed709c0d4b94b76ece5f2e39263
Zbeyde Özge YILMAZ <zozgeyilmaz@hotmail.com>
2021-02-08 19:13:03 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        List<Musteri> musteriler = new List<Musteri>();
+        public void Ekle(Musteri musteri)
+        {
+            musteriler.Add(musteri);
+            Console.WriteLine(musteri.Ad +" eklendi.");
+        }
+        public void Sil(Musteri musteri)
+        {
+            musteriler.Remove(musteri);
+            Console.WriteLine(musteri.Ad + " silindi.");
+        }
+        public void Listele()
+        {
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Id +" "+ musteri.Ad +" "+ musteri.Soyad +" "+ musteri.Sehir);
+            }
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MusteriManager musteriManager = new MusteriManager();
+            Musteri musteri1 = new Musteri {
+                Id = 1,
+                Ad = "Ali",
+                Soyad = "Yılmaz",
+                Sehir = "Kocaeli"
+            };
+            Musteri musteri2 = new Musteri {
+                Id = 1,
+                Ad = "Ayşe",
+                Soyad = "Öz",
+                Sehir = "İstanbul"
+            };
+            Musteri musteri3 = new Musteri{
+                Id = 1,
+                Ad = "Burak",
+                Soyad = "Kılıç",
+                Sehir = "Bursa"
+            };
+            
+
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri3);
+            Console.WriteLine("Musteriler Listeleniyor.....");
+            musteriManager.Listele();
+            
+            musteriManager.Sil(musteri2);
+            Console.WriteLine("Musteriler Listeleniyor.....");
+            musteriManager.Listele();
+
+            
+
+        }
+    }
+}
